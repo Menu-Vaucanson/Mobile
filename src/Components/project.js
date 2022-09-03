3f04adb9a64fa535d2ab627439cbe0d81e4944f2
unel <enee.prog@gmail.com>
2022-09-03 17:23:29 +0200

    add contributor

--- src/Components/project.js
+++ src/Components/project.js
@@ -1,12 +1,31 @@
 import ProjectLight from '../Themes/Project/Light';
 import ProjectDark from '../Themes/Project/Dark';
+import unel from '../Assets/unel.png'
+import wiwok from '../Assets/wiwok.png'
 
 function motivation() {
     return (
         <div className='motivation'>
-            <div className="motivation-title">motivation</div><div className="motivation-text">Bonjour a tous,
-                Nous avons créer ce site, car chaque matin, nous allions consulter le menu... avant de l’oublier quelques instants plus tard.
-                Ce site a donc été créer dans l’optique d’être un menu de poche, pratique à l’utilisation.
+            <div className="motivation-title">motivation</div><div className="motivation-text">Bonjour a tous,<br></br>
+                Nous avons créer ce site, car chaque matin, nous allions consulter le menu... avant de l’oublier quelques instants plus tard.<br></br>Ce site a donc été créer dans l’optique d’être un menu de poche, pratique à l’utilisation.
+            </div>
+        </div>
+    )
+}
+
+function Contributors() {
+    return (
+        <div className='contributors-box'>
+            <div className='contributors-title'>contributeurs</div>
+            <div className='contributorBox'>
+                <img src={unel} className="icon" alt={'icon unel'} />
+                <div className='contributor-name'>unel</div>
+                <div className='contributor-description'>Développeur, Designer</div>
+            </div>
+            <div className='contributorBox'>
+                <img src={wiwok} className="icon" alt={'icon unel'} />
+                <div className='contributor-name'>wiwok</div>
+                <div className='contributor-description'>Développeur, Designer</div>
             </div>
         </div>
     )
@@ -14,7 +33,10 @@ function motivation() {
 
 function Project({ theme }) {
     return (
-        motivation()
+        <div className='project-box'>
+            {motivation()}
+            {Contributors()}
+        </div>
     )
 }
 
